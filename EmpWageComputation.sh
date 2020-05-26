9dfcac1bedc9a1ccd1e04d405feb360984e81a80
Sippora04 <sippora.toppo95@gmail.com>
2020-05-26 15:39:47 +0530

    modified 2nd function

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,17 +1,16 @@
-xIS_PART_TIME=1;
+IS_PART_TIME=1;
 IS_FULL_TIME=2;
-#MAX_HRS_IN_MONTH=4;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
 totalEmpHr=0;
 totalWorkingDays=0;
 
-#declare -A dailyWage
+declare -A dailyWage
 
 function getWorkHrs()
 {
-   case $1 in   #$1=0
+   case $1 in
          $IS_FULL_TIME)
                   empHrs=8
                   ;;
@@ -26,8 +25,8 @@ function getWorkHrs()
 }
 
 function getEmpWage() {
-        local empHrs=$1
-        echo $(($empHrs*$EMP_RATE_PER_HR))
+        local empHrsIs=$1
+        echo $(($empHrsIs*$EMP_RATE_PER_HR))
 }
 echo "Welcome to employee Wage computation Program"
 echo "Day No.			DailyWage		TotalWage"
